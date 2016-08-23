0959f8cb703b0f39f96ffe380b542bac3a7aab3b
miguel456 <migueljfnogueira@gmail.com>
2016-08-23 04:52:02 +0100

    Add array for mysql connection

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Begin config file
 
-$configVariables = array("address1" => "0.0.0.0", "address2" => "1.0.0.0", "address3" => "2.0.0.0", "address4" => "3.0.0.0" );
+$whitelistedAddresses = array("address1" => "0.0.0.0", "address2" => "1.0.0.0", "address3" => "2.0.0.0", "address4" => "3.0.0.0" );
 // TODO: Use spacing to create multiline array (Better readable).
-
+$databaseDetails = array("host" => "", "password" => "", "database" => "", "username" => "");
 ?>
