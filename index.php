bc9ac597ee17b16849cf13a6acb2aed34604458d
miguel456 <migueljfnogueira@gmail.com>
2016-08-24 03:31:46 +0100

    Fixed bug two minor bugs
    Fixed bug where "nbsp" would not be parsed as html and logged address would not display to the "visitor".

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require 'capture.php'; // Most of the code that logs the user's IP address is on
     <body>
         <div align="center">
             <?php logAddress(); ?>
-            <p>Logging <?php $_SERVER['REMOTE_ADDR']; ?>nbsp;... </p>
+            <p>Logging <?php echo $_SERVER['REMOTE_ADDR']; ?>&nbsp;... </p>
             <p>Attempting to cleanse logs...</p>
             <?php deleteBigFile(); // Check log file and determine whether it is elegible for deletion ?>
         </div>
