395210149972b25c071423913bc2aa946b328f71
miguel456 <migueljfnogueira@gmail.com>
2016-08-27 02:23:38 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,16 @@
 $whitelistedAddresses = array("address1" => "0.0.0.0", "address2" => "1.0.0.0", "address3" => "2.0.0.0", "address4" => "3.0.0.0" );
 // TODO: Use spacing to create multiline array (Better readable).
 $databaseDetails = array("host" => "", "password" => "", "database" => "", "username" => "");
+
+/**
+* PHP Address Logger - The PHP IP logger and Honeypot - Copyright (C) 2016 miguel456
+*
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+* Full license: LICENSE.md within this repo.
+* Author may be reached via e-mail: miguel456@worldofdiamondsmail.us.to
+*/
 ?>
