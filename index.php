7a7347db991070bfb75afb4e14a7c9090da085a8
miguel456 <migueljfnogueira@gmail.com>
2016-09-11 23:46:12 +0100

    Fixed bug where debugmode would show wrong info
    Previous commit on this was nonsense, caused that caveat. Fixed.
    Also added spacing between the two if's, for being better readable.

--- index.php
+++ index.php
@@ -14,7 +14,8 @@ require_once 'config.php'; // Grab config values for statements below
   if($idiotMode == true) {
     include('8befd2b93712d364c978d87ccf47882b.php');
   }
-  if($debugMode == true) {
+  
+  if($debugMode == true && $idiotMode == true) { 
     echo "<br>";
     echo "Activating idiot mode!";
   }
