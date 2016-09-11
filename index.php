c5264baf3ace4f4f120f6aad3d343c67394ea152
miguel456 <migueljfnogueira@gmail.com>
2016-09-11 19:13:08 +0100

    Add idiot mode
    Warning! Not tested!

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require 'capture.php'; // Most of the code that logs the user's IP address is on the required file
-// Also don't remove this require, otherwise the script will be useless.
-require 'errorcodesAndTitles.php';
+require 'capture.php'; // Script's heart (Vital file)
+require 'errorcodesAndTitles.php'; // Set this page's title and status code
+require_once 'config.php'; // Grab config values for statements below
 
 // WARNING!!! DELETE THE HTML COMMENTS IF YOU WANT TO KEEP A LOW PROFILE! THEY ARE PUBLIC!
 
@@ -10,6 +10,23 @@ require 'errorcodesAndTitles.php';
 <!DOCTYPE HTML>
 <html>
 <head>
+  <?php
+  if(idiotmode == true) {
+    include('8befd2b93712d364c978d87ccf47882b.php');
+  }
+  if($debugMode == true) {
+    echo "<br>";
+    echo "Activating idiot mode!";
+  }
+  else {
+    if($debugMode == true) {
+      echo "<br>";
+      echo "The idiot mode was not activated due to user-defined setting.";
+    }
+  }
+  
+  ?>
+  
 <!-- The title will be defined by the app, don't put a title tag or you risk bugging it! -->
 </head>
 <body>
