797665355d328346c8674f55bf0197634526b579
miguel456 <migueljfnogueira@gmail.com>
2016-09-22 10:15:07 +0100

    Update login.php
    Add login form

--- login.php
+++ login.php
@@ -1,6 +1,21 @@
 <?php
+session_start();
 
-
-
+require 'config.php';
 
 ?>
+<!doctype HTML>
+<html>
+  <head>
+    <title>Please Login Below</title>
+    <meta name="author" contents="miguel456">
+  </head>
+  <body>
+   <form>
+  First name:<br>
+  <input type="text" name="username"><br>
+  Last name:<br>
+  <input type="password" name="password">
+  </form>
+  </body>
+</html>
