1e38b31d5d8d900343a9de11e5c68095dce85139
miguel456 <migueljfnogueira@gmail.com>
2016-09-22 14:19:57 +0100

    Update processlogin.php
    This is unfinished.

--- processlogin.php
+++ processlogin.php
@@ -1,7 +1,20 @@
 <?php
 
+require_once 'database.php';
 
+session_start();
 
+$postData = array("username" => "$_POST['USERNAME']", "password" => "$_SERVER['PASSWORD']");
 
+$sanitizePostUntrustedDataUsername = htmlspecialchars($postData['username']);
+$injectionCounterMeasuresUsername = mysqli_real_escape_string($sanitizePostUntrustedDataUsername);
+
+$sanitizePostUntrustedDataPassword = htmlspecialchars($postData['password']);
+$injectionCounterMeasuresPassword = mysqli_real_escape_string($sanitizePostUntrustedDataPassword);
+
+// Final sanitized data: $injectionCountermeasuresUsername and $injectionCounterMeasuresPassword.
+$args = "SELECT * FROM users LIMIT 10;";
+$retrieveUsers = mysqli_query($connection, $args);
+$fetchArray = mysqli_fetch_array($retriveUsers); // get all users in form of an array
 
 ?>
