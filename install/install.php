66c3a6ce3d474e3ffe2c587176f0829b96677431
miguel456 <migueljfnogueira@gmail.com>
2016-09-23 23:39:36 +0100

    Update install.php

--- install/install.php
+++ install/install.php
@@ -1,5 +1,13 @@
 <?php
+$postVars = array("email" => "$_POST['email']", 
+"username" => "$_POST['username']",
+"password" => "$_POST['password']",
+"name" => "$_POST['name']",
+"email" => $_POST['email'],
+);
 
+$whippedCleanNick = mysqli_real_escape_string($postVars['username']);
+$whippedClean = "";
 
 
 ?>
