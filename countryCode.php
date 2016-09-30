020a7975e9f0956954c586ad40d2526f6e55d29e
miguel456 <migueljfnogueira@gmail.com>
2016-09-30 10:16:05 +0100

    create country code function

--- countryCode.php
+++ countryCode.php
@@ -1,8 +1,16 @@
 <?php
 // Get user's country code, for displaying multillingual webpages.
 
+require_once 'config.php';
 
-
-
-
+function getCountryCode($hostnameOrIP) {
+	
+	$countryCode = geoip_country_code_by_name($hostnameOrIP);
+	global $debugMode = $debug; // may not work
+	if($debug == true) {
+		echo "Retrieved country code:" . $countryCode;
+	}
+	return $countryCode;
+}
+  
 ?>
