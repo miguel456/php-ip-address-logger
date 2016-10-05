474bd63a991ee4c5bf89f7c2ee7f4552e0b1644d
miguel456 <migueljfnogueira@gmail.com>
2016-10-05 11:00:46 +0100

    Set cookies for later use

--- setUserLang.php
+++ setUserLang.php
@@ -3,13 +3,76 @@ require_once 'errorcodesAndTitles.php';
 require 'countryCode.php';
 require_once 'config.php';
 
-if($debugMode == true) {
-  $debugFunc = getCountryCode($ipMethod, 1);
-} 
-else {
-  $nodebugFunc = getCountryCode($ipMethod, 0);
-}
+$nodebugFunc = getCountryCode($ipMethod, 0);
 
-switch($nodebugFunc)
+switch($nodebugFunc) {
+  case "PT":
+    if($debugMode == true) {
+      echo "<br>";
+      echo "User language is " . $nodebugFunc . ". Setting language to Portuguese.";
+      echo "<br>";
+    }
+    setcookie("language", "PT");
+    break;
+  case "US":
+    if($debugMode == true) {
+      echo "<br>";
+      echo "User language is " . $nodebugFunc . ". Setting language to American English.";
+      echo "<br>";
+    }
+    setcookie("language", "en-US");
+    break;
+  case "GB":
+    if($debugMode == true) {
+      echo "<br>";
+      echo "User language is " . $nodebugFunc . ". Setting user language to British English.";
+      echo "<br>";
+    }
+    setcookie("language", "en-GB");
+    break;
+  case "CN":
+    if($debugMode == true) {
+      echo "<br>";
+      echo "User language is " . $nodebugFunc . ". Setting user language to Chinese.";
+      echo "<br>";
+    }
+    setcookie("language", "CN");
+    break;
+  case "FR":
+    if($debugMode == true) {
+      echo $linebreak;
+      echo "User language is " . $nodebugFunc . ". Setting user language to French.";
+      echo $linebreak;
+    }
+    setcookie("language", "FR");
+    break;
+  case "DE":
+    if($debugMode == true) {
+      echo $linebreak;
+      echo "User language is " . $nodebugFunc . ". Setting user language to German/Deutsch.";
+      echo $linebreak;
+    }
+    setcookie("language", "DE");
+    break;
+  case "RU":
+    if($debugMode == true) {
+      echo $linebreak;
+      echo "User language is " . $nodebugFunc . ". Setting user language to Russian.";
+      echo $linebreak;
+    }
+    setcookie("language", "RU");
+    break;
+  case "BR":
+    setcookie("language", "PT");
+    break;
+  default:
+    if($debugMode == true) {
+      echo $linebreak;
+      echo "User language was not recognised, using/setting default language (en-US).";
+      echo $linebreak;
+    }
+    setcookie("language", "en-US");
+    break;
+}
 
 ?>
