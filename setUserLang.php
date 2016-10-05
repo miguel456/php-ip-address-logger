337b6a2a383cfbe5a3e97c62585cb8bbfe5b5c64
miguel456 <migueljfnogueira@gmail.com>
2016-10-05 10:12:32 +0100

    Update setUserLang.php

--- setUserLang.php
+++ setUserLang.php
@@ -1,5 +1,12 @@
 <?php
+require_once 'errorcodesAndTitles.php';
+require 'countryCode.php';
+require_once 'config.php';
 
-
+if($debugMode == true) {
+  getCountryCode($ipMethod, 1);
+} else {
+  getCountryCode($ipMethod, 0);
+}
 
 ?>
