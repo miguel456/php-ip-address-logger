9f7babefade0cf6329fd67c87abc08bab9a51ea0
miguel456 <migueljfnogueira@gmail.com>
2016-10-05 10:17:42 +0100

    Update setUserLang.php

--- setUserLang.php
+++ setUserLang.php
@@ -4,9 +4,12 @@ require 'countryCode.php';
 require_once 'config.php';
 
 if($debugMode == true) {
-  getCountryCode($ipMethod, 1);
-} else {
-  getCountryCode($ipMethod, 0);
+  $debugFunc = getCountryCode($ipMethod, 1);
+} 
+else {
+  $nodebugFunc = getCountryCode($ipMethod, 0);
 }
+
+switch($nodebugFunc)
 
 ?>
