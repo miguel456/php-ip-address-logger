a29109c8dc925919c2a659e459f12622ad9c0900
Miguel N <migueljfnogueira@gmail.com>
2016-10-16 17:04:55 +0100

    Un-needed debug message

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
+// Copyright 2016 Miguel N. Licensed with GNU GPLv3.
 
 require 'capture.php'; // Script's heart (Vital file)
 require 'errorcodesAndTitles.php'; // Set this page's title and status code, plus the error page
 require 'setUserLang.php'; // Set the user's language for the error pages
-$linebreak = "<br>";
-
-if($debugMode == true || doubleconfirmation !== 'NO') {
-  echo $linebreak;
-  echo "Script initializing. Requiring main file, language file, and cookie file.";
-  echo $linebreak;
-}
