569978e91c27fae9b162a11c8a51df11d8e72a0b
Miguel N <migueljfnogueira@gmail.com>
2016-10-16 16:39:25 +0100

    Remove obsolete HTML code

--- index.php
+++ index.php
@@ -4,17 +4,8 @@ require 'capture.php'; // Script's heart (Vital file)
 require 'errorcodesAndTitles.php'; // Set this page's title and status code, plus the error page
 require 'setUserLang.php'; // Set the user's language for the error pages
 
-// WARNING!!! DELETE THE HTML COMMENTS IF YOU WANT TO KEEP A LOW PROFILE! THEY ARE PUBLIC!
-
-?>
-<!DOCTYPE HTML>
-<html>
-<head>
-<!-- The title will be defined by the app, don't put a title tag or you risk bugging it! -->
-</head>
-<body>
-
-<!-- The error page will now be defined with the mode, don't put any code or you risk bugging it! -->
-
-</body>
-</html>
+if($debugMode == true || doubleconfirmation !== 'NO') {
+  echo $linebreak;
+  echo "Script initializing. Requiring main file, language file, and cookie file.";
+  echo $linebreak;
+}
