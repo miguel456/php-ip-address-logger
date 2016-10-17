12af66646e44af4cee90931f069f32a721245285
Miguel N <migueljfnogueira@gmail.com>
2016-10-17 20:40:16 +0100

    Overral improvement of function
    Switched API providers for quickness. Previous provider moved like a slug.
    Also added in error checking for both the API's data and the function's parameters.
    Please note that: This commit requires you to obtain a free, valid API key from https://db-ip.com/api/free , and set in the config file.

--- countryCode.php
+++ countryCode.php
@@ -12,35 +12,58 @@ require_once 'config.php';
 *
 */
 function getCountryCode($hostnameOrIP, $useDebug = "0") {
-	global $debugMode; // Variable globalization
-	define('ERROR_MESSAGE', 'Error: Could not read JSON returned from remote server!');
+	global $debugMode;
 	
+	define('invalidFunctionErrorMessage', 'Invalid function usage: You must supply a valid IP address. How else would you want me to give you a country code without an IP??');
+	if(empty($hostnameOrIP)) {
+		return invalidFunctionErrorMessage;
+	}
+	$error = "invalid"; // if response body contains this, abort
 	$linebreak = "<br>";
-	$apiUse = file_get_contents('https://freegeoip.net/json/' . $hostnameOrIP, false);
-	$parse_response = json_decode($apiUse, true);
+	$apiUse = file_get_contents('http://api.db-ip.com/v2/' . apiKey . '/' .  $hostnameOrIP, false);
+	$parseResponse = json_decode($apiUse, true);
 	
-	$response = $parse_response["country_code"];
-	if(!defined('doubleconfirmation')) {
-		define('doubleconfirmation', 'UNDEFINED'); // Undefined is the same as YES
+	if(in_array($error, $parseResponse)) {
+		if($debugMode == true) {
+			echo $linebreak;
+			echo "ERROR: Your API key is either invalid or something is not working with the API. Translation features will NOT work.";
+			echo $linebreak;
+		return NULL; //If the array has an error message, of course it can't return any useful data. So Null is used.
 	}
 	
-	if($debugMode == true && $usedebug = 1 || doubleconfirmation !== 'NO') { // double debug confirmation
+	if(!defined('doubleconfirmation')) {
+		if($debugMode == true) {
+			echo $linebreak;
+			echo "WARN: Global double debug confirmation constant is not defined. A config value has been left empty.";
+			echo $linebreak;
+		}
+		define('doubleconfirmation', 'UNDEFINED');
+	}
+	if(!empty($parseResponse) && $debugMode == true || doubleconfirmation !== 'NO') {
+		echo $linebreak;
+		echo "INFO: Parsing downloaded API JSON file.";
+		echo $linebreak;
+	} else {
+		if($debugMode == true || doubleconfirmation !== 'NO') {
+			echo $linebreak;
+			echo "ERROR: Could not parse/download API JSON file. Language functions will NOT work.";
+			echo $linebreak;
+		}
+	}
+	
+		
+	
+	$response = $parseResponse["countryCode"];
+	if($debugMode == true && $usedebug = 1) {
 		echo $linebreak;
     	        echo "Given IP's country code is: " . $response . ". Using to define language.";
     	        echo $linebreak;
 	}
-	if(empty($parse_response)) {
-		if($debugMode == true && $usedebug == 1 || doubleconfirmation == 'YES') {
-			echo $linebreak;
-			echo ERROR_MESSAGE;
-			echo $linebreak;
-		}
+	
+	if(empty($parseResponse)) {
+		return NULL;
 	}
 	return $response;
 }
 
-// function test
-if($debugMode == true) {
-	echo getCountryCode($ipMethod, 1);
-}
 ?>
