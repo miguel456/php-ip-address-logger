d9477d916dd34e40c5eea874cccf80edae078417
Miguel N <migueljfnogueira@gmail.com>
2016-11-16 20:39:31 +0000

    code optimisation
    Function now only returns 2 letter country codes or null.

--- countryCode.php
+++ countryCode.php
@@ -11,55 +11,20 @@ require_once 'config.php';
 * 1 will output debug messages (i.e. api down) while 0 will remain silent
 *
 */
-function getCountryCode($hostnameOrIP, $useDebug = "0") {
-	global $debugMode;
+function getCountryCode($hostnameOrIP) {
 	
-	define('invalidFunctionErrorMessage', 'Invalid function usage: You must supply a valid IP address. How else would you want me to give you a country code without an IP??');
 	if(empty($hostnameOrIP)) {
-		return invalidFunctionErrorMessage;
+		return null;
 	}
 	$error = "invalid"; // if response body contains this, abort
-	$linebreak = "<br>";
 	$apiUse = file_get_contents('http://api.db-ip.com/v2/' . apiKey . '/' .  $hostnameOrIP, false);
 	$parseResponse = json_decode($apiUse, true);
 	
 	if(in_array($error, $parseResponse)) {
-		if($debugMode == true) {
-			echo $linebreak;
-			echo "ERROR: Your API key is either invalid or something is not working with the API. Translation features will NOT work.";
-			echo $linebreak;
-		}
-		return NULL;
+		return null;
 	}
-	
-	if(!defined('doubleconfirmation')) {
-		if($debugMode == true) {
-			echo $linebreak;
-			echo "WARN: Global double debug confirmation constant is not defined. A config value has been left empty.";
-			echo $linebreak;
-		}
-		define('doubleconfirmation', 'UNDEFINED');
-	}
-	if(!empty($parseResponse) && $debugMode == true || doubleconfirmation !== 'NO') {
-		echo $linebreak;
-		echo "INFO: Parsing downloaded API JSON file.";
-		echo $linebreak;
-	} else {
-		if($debugMode == true || doubleconfirmation !== 'NO') {
-			echo $linebreak;
-			echo "ERROR: Could not parse/download API JSON file. Language functions will NOT work.";
-			echo $linebreak;
-		}
-	}
-	
-		
 	
 	$response = $parseResponse["countryCode"];
-	if($debugMode == true && $usedebug = 1) {
-		echo $linebreak;
-    	        echo "Given IP's country code is: " . $response . ". Using to define language.";
-    	        echo $linebreak;
-	}
 	
 	if(empty($parseResponse)) {
 		return NULL;
