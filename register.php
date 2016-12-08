35215a8781c4ce9c582a4f40ed7690a6f9be9310
Miguel N <migueljfnogueira@gmail.com>
2016-12-08 17:21:41 +0000

    Create form
    Create the registration form

--- register.php
+++ register.php
@@ -13,5 +13,22 @@ require 'config.php';
       <h1>Welcome to <?php echo sitename ?> IP management dashboard</h1>
       <h3>Access is reserved to authorized users only!</h3>
       <p style="font-weight: bold">If you wish to become an authorized user, please <a href="mailto:<?php echo site_webmaster_email ?>">send us an email!</a></p>
-      
+      <br>
+      <br>
+      <form action="register_user.php" method="POST">
+        Username:<br>
+        <input type="text" name="username" value="urname12345" required><br>
+        Password:<br>
+        <input type="text" name="password" value="12445" required><br>
+        E-mail:<br>
+        <input type="email" name="email" value="myname@mydomain.com" required><br>
+        Additional information:<br>
+        <textarea rows="5" col="50">
+          Type in some additional information here such as why you are registering and by whom you were invited
+        </textarea>
+      </form>
+    </div>
+  </body>
+</html>
+
   
