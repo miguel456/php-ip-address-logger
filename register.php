7c1145ded78dc7c5a41201181fd86414f589f5b6
Miguel N <migueljfnogueira@gmail.com>
2016-12-08 15:35:54 +0000

    initial html

--- register.php
+++ register.php
@@ -1,1 +1,17 @@
+<?php
+require 'config.php';
+?>
 
+<!doctype HTML>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>User Registration</title>
+  </head>
+  <body>
+    <div align="center">
+      <h1>Welcome to <?php echo sitename ?> IP management dashboard</h1>
+      <h3>Access is reserved to authorized users only!</h3>
+      <p style="font-weight: bold">If you wish to become an authorized user, please <a href="mailto:<?php echo site_webmaster_email ?>">send us an email!</a></p>
+      
+  
