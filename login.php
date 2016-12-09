f050f6f0501a523654fcf5b9174c7443886bac98
Miguel N <migueljfnogueira@gmail.com>
2016-12-09 10:59:34 +0000

    Create login form

--- login.php
+++ login.php
@@ -1,1 +1,28 @@
+<?php
+session_start();
+require_once 'config.php';
 
+?>
+<!doctype HTML>
+<html>
+  <head>
+    <title><?php echo sitename ?> &ndash; PHP IP Address Logger</title>
+  </head>
+  <body>
+    <div align="center">
+	<h1>Access is reserved to restricted users only! If you wish to become an authorized user, please <a href="mailto:<?php echo site_webmaster_email ?>">send us an email!</a></h1>
+    <h3><?php echo sitename ?> IP management System &ndash; Login</h3>
+    <br>
+    <form name="login" method="POST" action="login_user.php"><br>
+      Username:
+      <input type="text" name="username" value="" required><br>
+      Password:
+      <input type="password" name="password" value="" required>
+      <br>Remember me?<br>
+      <input type="radio" name="rememberme" value="Yes"> Yes | No<input type="radio" name="radio" value="No">
+      <br>
+      <input type="submit" name="submit" value="Login">
+    </form>
+	</div>
+   </body>
+  </html>
