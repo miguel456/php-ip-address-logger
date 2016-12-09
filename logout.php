b8f2c5720d6b1bda78f1c94c5ec772637c19e132
Miguel N <migueljfnogueira@gmail.com>
2016-12-09 14:06:53 +0000

    Add logout file

--- logout.php
+++ logout.php
@@ -1,2 +1,13 @@
 <?php
 // destroy and unset session
+if($_SESSION['isLoggedIn'] == false) {
+    header("HTTP/1.1 400 Bad Request");
+    header("Location: login.php");
+} elseif($_SESSION['isLoggedIn'] == true) {
+    echo "Logging user " . $_SESSION['username'] . "out. Goodbye! ";
+    session_unset();
+    session_destroy();
+    unset($_SESSION['id']);
+    header("Location: login.php");
+} else
+    echo "Unknown error while trying to sign you out. Please try logging in again.";
