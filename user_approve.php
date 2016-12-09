8f557c7a0d920fbaaead832d538b83be60807257
Miguel N <migueljfnogueira@gmail.com>
2016-12-09 14:15:07 +0000

    Update user_approve.php

--- user_approve.php
+++ user_approve.php
@@ -1,2 +1,14 @@
 <?php
 session_start();
+require 'database.php';
+
+// authentication
+function checkIfLoggedIn($username) { // possible vulnerablility detected
+  if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true) {
+    return true;
+  }
+  else
+    return false;
+  }
+}
+//////////////////// UNFINISHED ////////////////////////////////
