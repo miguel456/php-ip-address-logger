864cc66a32ede5d69eef597555f6701090b72c7f
Miguel N <migueljfnogueira@gmail.com>
2016-12-10 12:53:37 +0000

    Update loginController.php

--- admin/controllers/loginController.php
+++ admin/controllers/loginController.php
@@ -4,7 +4,7 @@
 * Checks if the username session variable is set and that isLoggedIn is true.
 */
 function sessionAuthorize($username) { // possible vulnerablility detected
-  if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true) {
+  if(isset($_SESSION['isLoggedIn']) && $_SESSION['username'] = $username && $_SESSION['isLoggedIn'] == true) {
     $_SESSION['authorizedByEndCode'] = "true";
     setcookie("loginAuthorized", "true"); 
     return true;
