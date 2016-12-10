31a068ace14bbdbd16b7b83603bc08e7792cc2ef
Miguel N <migueljfnogueira@gmail.com>
2016-12-10 22:14:27 +0000

    Logout enhancements

--- admin/logout/index.php
+++ admin/logout/index.php
@@ -1,13 +1,21 @@
 <?php
 // destroy and unset session
-if($_SESSION['isLoggedIn'] == false) {
+session_start();
+if($_SESSION['isLoggedIn'] == false && !isset($_COOKIE['loginAuthorized']) && $_SESSION['authorizedByEndCode'] == null) {
     header("HTTP/1.1 400 Bad Request");
-    header("Location: login.php");
+    header("Location: " . domainRoot . "/" . appRoot . "/admin/login");
 } elseif($_SESSION['isLoggedIn'] == true) {
-    echo "Logging user " . $_SESSION['username'] . "out. Goodbye! ";
+    header("HTTP/1.1 200 0K");
     session_unset();
     session_destroy();
     unset($_SESSION['id']);
-    header("Location: login.php");
-} else
-    echo "Unknown error while trying to sign you out. Please try logging in again.";
+    header("Location: " . domainRoot . "/" . appRoot . "/admin/login");
+} else {
+    header("HTTP/1.1 500 Internal Server Error");
+    header("Location: " . domainRoot . "/" . appRoot . "/admin "); // let other part of script determine what's going on
+    if($debugMode == true) {
+        echo "<br>";
+        echo "An error has ocurred while attempting to log user " . $_SESSION['username'] . " out.";
+    }
+}
+
