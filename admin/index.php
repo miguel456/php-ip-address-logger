73cdd00c83be9376fbd068f79bd9faaac6a5c233
Miguel N <migueljfnogueira@gmail.com>
2016-12-10 13:53:06 +0000

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require 'controllers/loginController.php';
-sessionVerify();
+accessVerify();
 
 echo "<p>Welcome to the control panel, " . $_SESSION['username'] . "! Your session has been approved, as your login credentials were correct. </p>";
 echo '<br><p>Available actions: <a href="approve_user.php">Approve an user</a></p>';
