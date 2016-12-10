8a915dc037f02fc09ed2f7ee7385c33fa443cbc8
Miguel N <migueljfnogueira@gmail.com>
2016-12-10 22:20:57 +0000

    config file path

--- admin/logout/index.php
+++ admin/logout/index.php
@@ -1,18 +1,24 @@
 <?php
 // destroy and unset session
+require_once '../config.php';
 session_start();
+
 if($_SESSION['isLoggedIn'] == false && !isset($_COOKIE['loginAuthorized']) && $_SESSION['authorizedByEndCode'] == null) {
-    header("HTTP/1.1 400 Bad Request");
-    header("Location: " . domainRoot . "/" . appRoot . "/admin/login");
+         header("HTTP/1.1 400 Bad Request");
+         header("Location: " . domainRoot . "/" . appRoot . "/admin/login");
 } elseif($_SESSION['isLoggedIn'] == true) {
-    header("HTTP/1.1 200 0K");
-    session_unset();
-    session_destroy();
-    unset($_SESSION['id']);
-    header("Location: " . domainRoot . "/" . appRoot . "/admin/login");
+        
+        header("HTTP/1.1 200 0K");
+        session_unset();
+        session_destroy();
+        unset($_SESSION['id']);
+        header("Location: " . domainRoot . "/" . appRoot . "/admin/login");
+  
 } else {
+    
     header("HTTP/1.1 500 Internal Server Error");
     header("Location: " . domainRoot . "/" . appRoot . "/admin "); // let other part of script determine what's going on
+    
     if($debugMode == true) {
         echo "<br>";
         echo "An error has ocurred while attempting to log user " . $_SESSION['username'] . " out.";
