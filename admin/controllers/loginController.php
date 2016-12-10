c952d86cccb41f6dbaa1cba2a7d9dec7562ce18f
Miguel N <migueljfnogueira@gmail.com>
2016-12-10 12:51:53 +0000

    Update loginController.php

--- admin/controllers/loginController.php
+++ admin/controllers/loginController.php
@@ -24,6 +24,6 @@ function isAuthorized() {
 function accessVerify(){
   if(isAuthorized() !== true) {
     header("HTTP/1.1 403 Forbidden");
-    exit('<h1 align="center">Access denied: You must login to view this page.</h1>');
+    header("Location: index.php");
   }
 }
