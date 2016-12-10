c0bd9c6ffba2a19cff61216e70a1b42789f8157e
Miguel N <migueljfnogueira@gmail.com>
2016-12-10 12:27:59 +0000

    Update loginController.php

--- admin/loginController.php
+++ admin/loginController.php
@@ -4,8 +4,10 @@
 * This function will return true if the user is logged in to the admin panel. Will return false if otherwise.
 * Checks if the username session variable is set and that isLoggedIn is true.
 */
-function checkIfLoggedIn($username) { // possible vulnerablility detected
+function sessionAuthorize($username) { // possible vulnerablility detected
   if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true) {
+    $_SESSION['authorizedByEndCode'] = "true";
+    setcookie("loginAuthorized", "true"); 
     return true;
   }
   else
