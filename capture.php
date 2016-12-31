2567c9dc4803850a3569b6eed41f2c19f8beca71
Miguel N <migueljfnogueira@gmail.com>
2016-12-31 23:34:52 +0000

    Update capture.php

--- capture.php
+++ capture.php
@@ -8,7 +8,7 @@
 * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
-* Full license: LICENSE.md within this repo.
+* Full license: LICENSE.md
 * Author may be reached via e-mail: miguel456@worldofdiamondsmail.us.to
 */
 
